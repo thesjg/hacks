3cb84a5452eac307e89ee95b079b9a29556ca21c
Samuel J. Greear <sjg@evilcode.net>
2009-02-04 11:13:46 -0800

    Clean up #defines

--- ecached/error.h
+++ ecached/error.h
@@ -5,20 +5,17 @@
 #include <sysexits.h>
 
 
-#define ecached_err(code, fmt)		\
-    do {				\
-        err(code, fmt);			\
-    } while (0)
+#define ecached_err(code, fmt) do {		\
+    err(code, fmt);				\
+} while (0)
 
-#define ecached_errx(code, fmt, ...)	\
-    do {				\
-        errx(code, fmt, __VA_ARGS__);	\
-    } while (0)
+#define ecached_errx(code, fmt, ...) do {	\
+    errx(code, fmt, __VA_ARGS__);		\
+} while (0)
 
-#define ecached_warn(fmt)		\
-    do {				\
-        warn(fmt);			\
-    } while (0)
+#define ecached_warn(fmt) do {			\
+    warn(fmt);					\
+} while (0)
 
 
 #endif /* _ECACHED_ERROR_H_ */
