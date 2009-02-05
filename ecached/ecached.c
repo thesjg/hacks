7a9406110fe38adb6cdcc08579415b9e3814b35d
Samuel J. Greear <sjg@evilcode.net>
2009-02-04 22:57:22 -0800

    getopt(3) preliminaries

--- ecached/ecached.c
+++ ecached/ecached.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "error.h"
 #include "cache.h"
 #include "hash.h"
+#include "network.h"
 
+
+static void
+usage(void)
+{
+    (void)fprintf(stderr, "Usage:\n%s%s%s%s%s%s%s%s",
+"-p <port>         TCP port\n",
+"-l <address>      Listen address\n",
+"-m <megabytes>    Maximum memory usage\n",
+"-c <connections>  Maximum connections\n",
+"-v [<level>]      Verbosity\n",
+"-P <filename>     PID file\n",
+"-t                Use threads\n",
+"-h                This help\n");
+
+    exit(EX_USAGE);
+}
 
 int
 main(int argc, char *argv[])
 {
+    int opt;
+    bool pFlag, lFlag, dFlag, mFlag, cFlag, vFlag, PFlag, tFlag = false;
+
     hash_entry_t *he;
 
     cache_init();
@@ -16,6 +38,53 @@ main(int argc, char *argv[])
     he = hash_search("abcd", 4);
     if (he != NULL)
         printf("%s\n", he->key);
+
+    while ((opt = getopt(argc, argv, "p:l:dm:c:v:P:t:h")) != -1) {
+        switch (opt) {
+        case 'p':
+            /* TCP port number */
+            pFlag = true;
+//            port = optarg;
+            break;
+        case 'l':
+            /* IP address */
+            lFlag = true;
+            break;
+        case 'd':
+            /* Daemonize */
+            dFlag = true;
+            break;
+        case 'm':
+            /* Max memory usage */
+            mFlag = true;
+            break;
+        case 'c':
+            /* Max simultaneous connections */
+            cFlag = true;
+            break;
+        case 'v':
+            /* Verbose [level] */
+            vFlag = true;
+            break;
+        case 'P':
+            /* PID file */
+            PFlag = true;
+            break;
+        case 't':
+            /* Threaded */
+            tFlag = true;
+            break;
+        case 'h':
+            /* Help */
+        default:
+            usage();
+        }
+    }
+    argc -= optind;
+    argv += optind;
+
+    if (dFlag == true)
+        daemon(0, (vFlag == true) ? 1 : 0);
 
     network_main();
 
