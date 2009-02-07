4e8ad8a93e00ea8094620fb6fdb2ad70fecdb530
Samuel J. Greear <sjg@evilcode.net>
2009-02-06 23:27:51 -0800

    Pull out protocol-specific bits, firm up the rest

--- ecached/network.h
+++ ecached/network.h
@@ -12,51 +12,23 @@
 
 #include "error.h"
 #include "memory.h"
+#include "protocol.h"
 #include "utils.h"
 
 
-typedef enum {
-    COMMAND_SET,
-    COMMAND_ADD,
-    COMMAND_REPLACE,
-    COMMAND_APPEND,
-    COMMAND_PREPEND,
-    COMMAND_CAS,
-
-    COMMAND_GET,
-    COMMAND_GETS
-} network_command_t;
-
-typedef enum {
-    CONNECTION_ACCEPTED,
-    CONNECTION_PARSED_COMMAND,
-
-} network_connection_state_t;
-
-typedef struct network_mod_action {
-    .				key,
-    uint32_t			flags,
-    time_t			exptime, /* ? */
-    .				bytes,
-    boolean			noreply    
-} network_mod_action_t;
-
-typedef struct network_get_action {
-    key(s)
-
-} network_get_action_t;
-
-typedef struct network_action {
-    network_command_t		command,
-    union {
-        network_mod_action_t
-        network_get_action_t
-    }
-} network_action_t;
+typedef struct network_buffer {
+    ssize_t	size;
+    ssize_t	used;
+    char	buffer[];
+} network_buffer_t;
 
 typedef struct network_connection {
-    network_connection_state_t	state,
-    network_action_t		action
+    enum {
+        CONNECTION_ACCEPTED,
+        CONNECTION_PARSED_COMMAND,
+    } state;
+    network_buffer_t	*buffer;
+    protocol_action_t	paction;
 } network_connection_t;
 
 
